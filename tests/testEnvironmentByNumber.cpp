2f1e20ded3f18ba11693bc0b6dfbbcc6995e74e8
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2016-12-31 22:27:23 -0500

    EnvironmentByNumber tested with the new interface
    	modified:   tests/testEnvironmentByNumber.cpp

--- tests/testEnvironmentByNumber.cpp
+++ tests/testEnvironmentByNumber.cpp
@@ -11,6 +11,9 @@ int main(int argc, char** argv)
 	e0.getParameters("11 3");
 	std::cout << e0.getDesc() << std::endl;
 
+	e0.setEvaluation(1000.);
+	std::cout << e0.getEvaluation() << std::endl;
+
 	EnvironmentByNumber e1(hyp);
 //	e1.getParameters("12");
 //	e1.getParameters("13 4 5");
